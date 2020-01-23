3caf6aa3d89d7f5eb64f6c0ed752a86566e305da
samsul <samsuladry@gmail.com>
2020-01-23 16:44:31 +0800

    some changes

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = 
+{
+    apiKey: "AIzaSyCjfuvQknOPvUPejaOrvi3KQKcGfyUxaFA",
+    authDomain: "eelmoo-70351.firebaseapp.com",
+    databaseURL: "https://eelmoo-70351.firebaseio.com",
+    projectId: "eelmoo-70351",
+    storageBucket: "eelmoo-70351.appspot.com",
+    messagingSenderId: "817460638819",
+    appId: "1:817460638819:web:9e288e87bee1d575513456",
+    measurementId: "G-FV3BDXRZ2N"
+  };
